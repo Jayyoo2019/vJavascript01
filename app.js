345833605a0c8e2a28986a6368b0293076b4cbf2
Jayyoo2019 <53619923+Jayyoo2019@users.noreply.github.com>
2021-10-09 00:09:57 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
+  
   const randomNumber = getRandomNumber();
-  // console.log(randomNumber);
+  console.log(randomNumber);
 
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
+  console.log(color.textContent)
 });
 
 function getRandomNumber() {
   return Math.floor(Math.random() * colors.length);
+  
 }
--- hex.js
+++ hex.js
@@ -1,18 +1,25 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
-
+const name = document.getElementById("name");
 btn.addEventListener("click", function () {
   let hexColor = "#";
+  
+  //console.log(hexColor);
   for (let i = 0; i < 6; i++) {
     hexColor += hex[getRandomNumber()];
   }
-  // console.log(hexColor);
+
+  console.log("hello"+getRandomNumber())
+  console.log(hexColor)
+  //console.log(hexColor);
 
   color.textContent = hexColor;
+  name.style.color = hexColor;
   document.body.style.backgroundColor = hexColor;
 });
 
 function getRandomNumber() {
+  console.log(Math.floor(Math.random() * hex.length))
   return Math.floor(Math.random() * hex.length);
 }
